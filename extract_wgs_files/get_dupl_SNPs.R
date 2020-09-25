41e520e8289bc5d95448c779d8a64965bb9a084f
dayam <dayam@Xs-MacBook-Air.local>
2020-09-25 13:51:24 -0600

    Deleted unnecessary script files

--- extract_wgs_files/get_dupl_SNPs.R
+++
@@ -1,8 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-in.file.name <- args[1]
-chr <- args[2]
-
-bim <- read.table(in.file.name, stringsAsFactors=F)
-dupl.snps <- bim$V2[duplicated(bim$V2)]
-
-write.table(dupl.snps, paste0("tmp_dupl_chr", chr, ".txt"),  sep="\t", quote=F, row.names=F, col.names=F)
