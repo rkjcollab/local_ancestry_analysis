ecffaaa01f737286ac5f00ed34f2243f83a88d2b
dayam <dayam@Xs-MacBook-Air.local>
2020-10-01 13:53:17 -0600

    Paramaterized create_classes.py

--- rfmix_file_creation_scripts/create_classes.py
+++ rfmix_file_creation_scripts/create_classes.py
@@ -1,13 +1,15 @@
 import os, sys
 
 out_file=open(sys.argv[1], "w")
-n=int(sys.argv[2])
+n_admixed=int(sys.argv[2])
+n_european=int(sys.argv[3])
+n_african=int(sys.argv[4])
 
-for i in range(0,n):
+for i in range(0,n_admixed):
     out_file.write("0 0 ")
-for i in range(0, 99):
+for i in range(0, n_european):
     out_file.write("1 1 ")
-for i in range(0, 108):
+for i in range(0, n_african):
     out_file.write("2 2 ")
 out_file.write("\n")
 out_file.close()
