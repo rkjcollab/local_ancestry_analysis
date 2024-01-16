abbe1aba5d0cf6541ca51ad48bd668ecf5a22e24
sdslack <sarah.slack@cuanschutz.edu>
2024-01-15 17:14:35 -0700

    Update to run all chromosomes

--- prep_input.sh
+++ prep_input.sh
@@ -5,56 +5,65 @@
 
 # Get args
 vcf_input=$1
-plink_output=$2
-out_dir=$3
-code_dir=$4
+out_dir=$2
+code_dir=$3
+
+# Set PLINK output
+plink_output_temp=$(basename "$vcf_input")
+plink_output_temp2="${plink_output_temp%.*}"
+plink_output="${plink_output_temp2%.*}"  # repeat for .vcf.gz
 
 # Prep input
-# TODO: this PLINK2 not present in container!
+# TODO: need to add PLINK2 to container, currently this is run in
+# batch script
 # plink2 \
 # 	--vcf "$vcf_input" \
 # 	--make-bed  --keep-allele-order \
 # 	--set-all-var-ids @:#:\$r:\$a \
-# 	--new-id-max-allele-len 67 \
-# 	--out "$plink_output"
+# 	--new-id-max-allele-len 100 \
+# 	--out "${out_dir}/${plink_output}"
 
-# TODO: for now, arbitrarily keeping first of duplicate positions
+# Prep input
+# Remove duplicate positions, keeping first (matches topmed_imputation repo)
 plink \
-	--bfile "$plink_output" \
+	--bfile "${out_dir}/${plink_output}" \
 	--list-duplicate-vars 'ids-only' 'suppress-first'\
-	--out "${plink_output}_dup_list"
+	--out "${out_dir}/tmp_${plink_output}_dup_list"
 
 plink \
-	--bfile "$plink_output" \
+	--bfile "${out_dir}/${plink_output}" \
 	--make-bed --keep-allele-order \
-	--exclude "${plink_output}_dup_list.dupvar" \
-	--out "${plink_output}_dedup"
+	--exclude "${out_dir}/tmp_${plink_output}_dup_list.dupvar" \
+	--out "${out_dir}/tmp_${plink_output}_dedup"
 
-# Based on code from topmed_imputation repo
-#Create bed file to crossover from hg19 to hg38 
-cat ${plink_output}_dedup.bim | cut -f1 | sed 's/^/chr/' > ${out_dir}/tmp_c1.txt
-cat ${plink_output}_dedup.bim | cut -f4 > ${out_dir}/tmp_c2.txt
-cat ${plink_output}_dedup.bim | cut -f4 > ${out_dir}/tmp_c3.txt
-cat ${plink_output}_dedup.bim | cut -f2 > ${out_dir}/tmp_c4.txt
-paste  ${out_dir}/tmp_c1.txt \
-       ${out_dir}/tmp_c2.txt \
-       ${out_dir}/tmp_c3.txt \
-       ${out_dir}/tmp_c4.txt \
-       >  ${out_dir}/tmp_in.bed
+# Liftover, based on code from topmed_imputation repo
+# Create bed file to crossover from hg38 to hg19
+cat "${out_dir}/tmp_${plink_output}_dedup.bim" | cut -f1 | sed 's/^/chr/' > "${out_dir}/tmp_c1.txt"
+cat "${out_dir}/tmp_${plink_output}_dedup.bim" | cut -f4 > "${out_dir}/tmp_c2.txt"
+cat "${out_dir}/tmp_${plink_output}_dedup.bim" | cut -f4 > "${out_dir}/tmp_c3.txt"
+cat "${out_dir}/tmp_${plink_output}_dedup.bim" | cut -f2 > "${out_dir}/tmp_c4.txt"
+paste  "${out_dir}/tmp_c1.txt" \
+       "${out_dir}/tmp_c2.txt" \
+       "${out_dir}/tmp_c3.txt" \
+       "${out_dir}/tmp_c4.txt" \
+       > "${out_dir}/tmp_in.bed"
 
-#Do crossover
+# Do crossover
 CrossMap.py bed ${code_dir}/shapeit_formatting_scripts/hg38ToHg19.over.chain \
-	${out_dir}/tmp_in.bed  \
-	${out_dir}/tmp_out.bed
+	"${out_dir}/tmp_in.bed"  \
+	"${out_dir}/tmp_out.bed"
 
-#Extract only those SNPs that were successfully cross-overed
-cut -f4 ${out_dir}/tmp_out.bed > ${out_dir}/tmp_snp_keep.txt
+# Extract only those SNPs that were successfully cross-overed
+cut -f4 "${out_dir}/tmp_out.bed" > "${out_dir}/tmp_snp_keep.txt"
 plink \
-	--bfile ${plink_output}_dedup \
-	--extract ${out_dir}/tmp_snp_keep.txt \
+	--bfile "${out_dir}/tmp_${plink_output}_dedup" \
+	--extract "${out_dir}/tmp_snp_keep.txt" \
 	--keep-allele-order \
-	--make-bed --out ${plink_output}_dedup_hg19
+	--make-bed --out "${out_dir}/${plink_output}_dedup_hg19"
 
-#Update bim file positions
+# Update bim file positions
 Rscript --vanilla ${code_dir}/update_pos.R \
-	${out_dir}/tmp_out.bed ${plink_output}_dedup_hg19.bim
+	"${out_dir}/tmp_out.bed" "${out_dir}/${plink_output}_dedup_hg19.bim"
+
+# Clean up
+rm ${out_dir}/tmp_*
