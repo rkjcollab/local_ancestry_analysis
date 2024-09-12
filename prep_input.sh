5dbc2fda29bb1d22d76f521afdd8086e4b01f8c4
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2024-09-12 06:59:34 -0600

    Update documentation

--- prep_input.sh
+++ prep_input.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Script preps input for local ancestry pipeline, currently
-# written to start from a single chromosome PLINK input in either
-# hg19 or hg38.
+if [ "$#" -eq  "0" ]
+then
+   echo "Usage: ${0##*/} <path_to_plink_prefix> <output_dir_prefix> <code_dir>"
+   echo "	<genome_build_of_input>"
+   echo "Script preps chip input for local ancestry pipeline. Input should be a"
+   echo "single-chromosome PLINK1.9 file in either hg19 or hg38."
+   echo "<genome_build_of_input> must either be 'hg19' or 'hg38'."
+   exit
+fi
 
 # Get args
 plink_input=$1
@@ -10,11 +16,17 @@ out_dir=$2
 code_dir=$3
 build=$4
 
+# Make sure genome build only hg19 or hg38
+if [[ "$build" != "hg19" && "$build" != "hg38" ]]; then
+	echo "Input build must be either 'hg38' or 'hg19'."
+	exit
+fi
+
 # Set PLINK output
 plink_output=$(basename "$plink_input")
 
 # Prep input
-# Remove duplicate positions, keeping first
+# Remove duplicate positions, keeping first (this is done for SHAPEIT)
 plink \
 	--bfile "${plink_input}" \
 	--keep-allele-order \
@@ -56,7 +68,7 @@ if [ "$build" == "hg38" ]; then
 		--make-bed --out "${out_dir}/${plink_output}_dedup_hg19"
 
 	# Update bim file positions
-	Rscript --vanilla ${code_dir}/update_pos.R \
+	Rscript --vanilla ${code_dir}/shapeit_formatting_scripts/update_pos.R \
 		"${out_dir}/tmp_out.bed" "${out_dir}/${plink_output}_dedup_hg19.bim"
 else
 	echo "Input build is already hg19."
