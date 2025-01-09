eb6fe25874c3354714bea7d0d9304a9d239272c9
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-01-08 20:01:10 -0700

    Update pheno to match prep script

--- laaa_fine_mapping/run_models/models_lung_function.R
+++ laaa_fine_mapping/run_models/models_lung_function.R
@@ -1,23 +1,23 @@
 runLaaaModelSummary <- function(model.frame) {
   form = as.character(paste(
-    pheno, "~", paste(cov_list, collapse = " + "), "+ allele_dose + afr_dose + allele_afr_dose"))
+    "pheno ~", paste(cov_list, collapse = " + "), "+ allele_dose + afr_dose + allele_afr_dose"))
   return (summary(lm(form, data=model.frame)))
 }
 
 runNullModel <- function(model.frame) {
   form = as.character(paste(
-    pheno, "~", paste(cov_list, collapse = " + ")))
+    "pheno ~", paste(cov_list, collapse = " + ")))
   return (lm(form, data=model.frame))
 }
 
 runLaaaModel <- function(model.frame) {
   form = as.character(paste(
-    pheno, "~", paste(cov_list, collapse = " + "), "+ allele_dose + afr_dose + allele_afr_dose"))
+    "pheno ~", paste(cov_list, collapse = " + "), "+ allele_dose + afr_dose + allele_afr_dose"))
   return (lm(form, data=model.frame))
 }
 
 runAlleleModelSummary <- function(model.frame) {
   form = as.character(paste(
-    pheno, "~", paste(cov_list, collapse = " + "), "+ allele_dose"))
+    "pheno ~", paste(cov_list, collapse = " + "), "+ allele_dose"))
   return (summary(lm(form, data=model.frame)))
 }
