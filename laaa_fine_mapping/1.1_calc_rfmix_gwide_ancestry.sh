b8596f1f0264998315017d33e9e82232ee564a88
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-01-10 13:34:52 -0700

    Add job script and use container

--- laaa_fine_mapping/1.1_calc_rfmix_gwide_ancestry.sh
+++ laaa_fine_mapping/1.1_calc_rfmix_gwide_ancestry.sh
@@ -5,14 +5,6 @@
 # before this step is run. Can be run using RFMix results run with collapse flag -o
 # or -co, but assumes RFMix was run in batches.
 
-# TO NOTE: update the input_file_name path to the same input used in the first step
-# of the local_anc_afr_eur subfolder:
-    # Unphased data: set input_file_name to plink_input in job_prep_input_unphased.batch,
-        # does not matter which chromosome or if all chromosomes concatenated
-    # Phased data: set input_file_name to input_vcf in job_prep_input_phased.batch,
-        # does not matter which chromosome or if all chromosomes concatenated
-# Also set the path to RFMix results split by chromosome (rfmix_results dir).
-
 # Scripts assume RFMix output file names are as output by RFMix:
     # chr#_local_ancestry.0.Viterbi.txt
     # chr#_local_ancestry.allelesRephased0.txt
@@ -21,17 +13,8 @@
     # chr#_local_ancestry_batch1.0.SNPsPerWindow.txt (if found in dir, means
         # RFMix was run with -co option)
 
-# Test imputed
-# input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/imp/chr22_small.vcf"
-# rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/imp_output"
-
-# Test chip
-# input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/chip/chr22_small"
-# rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/chip_output"
-
-# Test WGS
-input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/local_anc_afr_eur/wgs/chr10_small.vcf"
-rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/local_anc_afr_eur/wgs_output"
+input_file_name="$1"
+rfmix_results_dir="$2"
 
 # Get list of sample IDs from VCF or PLINK file
 if [[ "$input_file_name" == *.vcf* ]]; then
---
+++ laaa_fine_mapping/batch_files/job_1.1_calc_rfmix_gwide_ancestry.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# TO NOTE: update the input_file_name path to the same input used in the first step
+# of the local_anc_afr_eur subfolder:
+    # Unphased data: set input_file_name to plink_input in job_prep_input_unphased.batch,
+        # does not matter which chromosome or if all chromosomes concatenated
+    # Phased data: set input_file_name to input_vcf in job_prep_input_phased.batch,
+        # does not matter which chromosome or if all chromosomes concatenated
+# Also set the path to RFMix results split by chromosome (rfmix_results dir).
+
+# Test imputed
+# input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/imp/chr22_small.vcf"
+# rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/imp_output"
+
+# Test chip
+# input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/chip/chr22_small"
+# rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/chip_output"
+
+# Test WGS
+# input_file_name="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/local_anc_afr_eur/wgs/chr10_small.vcf"
+# rfmix_results_dir="${RKJCOLLAB}/Collabs/ortega/data/pipeline_test_data/local_anc_afr_eur/wgs_output"
+
+# Test BARD
+data_dir="/Users/slacksa/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Collabs/ortega/data/pipeline_test_data/bard_from_abhishek"
+code_dir="/Users/slacksa/repos/local_ancestry_analysis/laaa_fine_mapping"
+input_file_name="${data_dir}/imp/chr1.dose.first10krows.vcf"
+rfmix_results_dir="${data_dir}/imp_output"
+
+# Run script
+cont_dir=$(dirname "${code_dir}")
+apptainer exec --bind ${data_dir}:${data_dir} --bind ${code_dir}:${code_dir} \
+		${cont_dir}/local_ancestry_analysis.sif \
+ 		bash ${code_dir}/1.1_calc_rfmix_gwide_ancestry.sh \
+            "$input_file_name" "$rfmix_results_dir"
