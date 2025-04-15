ad57b15bb9dda650cb28ea10de6aea7f3191e607
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-04-15 12:58:07 -0600

    Update for SARP re-run, MAF after sample filtering, general clean up

--- local_anc_afr_eur/prep_input_phased.sh
+++ local_anc_afr_eur/prep_input_phased.sh
@@ -9,7 +9,6 @@ then
    exit
 fi
 
-
 # Get args
 vcf_input=$1
 out_dir=$2
@@ -17,39 +16,38 @@ min_maf=$3
 nr_threads=$4
 samp_ids=$5
 
-# Set VCF output
-vcf_output=`basename $vcf_input | sed 's/.vcf.gz//'`
+vcf_prefix=$(basename "$vcf_input")
+vcf_prefix="${vcf_prefix%.gz}"  # removes .gz if present
+vcf_prefix="${vcf_prefix%.vcf}"  # removes .vcf
 
 # Remove ID, INFO, FORMAT fields from VCF file
      # including format removes all tags except for GT
 bcftools  annotate -x ID,^INFO/R2,INFO/MAF,FORMAT \
-     "$vcf_input" -Oz -o "${out_dir}/tmp_${vcf_output}_noinfo.vcf.gz" \
+     "$vcf_input" -Oz -o "${out_dir}/${vcf_prefix}_noinfo.vcf.gz" \
      --threads $nr_threads
 
-# Because bcftools view with MAF filter is silent if MAF tag absent,
-# use bcftools to fill MAF values
-bcftools +fill-tags "${out_dir}/tmp_${vcf_output}_noinfo.vcf.gz" \
-     -Oz -o "${out_dir}/tmp_${vcf_output}_noinfo_maf.vcf.gz"
+# Filter to just samples of interest
+bcftools view -S "$samp_ids" \
+    "${out_dir}/${vcf_prefix}_noinfo.vcf.gz" \
+    -Oz -o "${out_dir}/tmp_${vcf_prefix}_noinfo_filt.vcf.gz"
+
+# Update MAF calculations after sample filtering
+bcftools +fill-tags "${out_dir}/tmp_${vcf_prefix}_noinfo_filt.vcf.gz" \
+     -Oz -o "${out_dir}/tmp_${vcf_prefix}_noinfo_filt_tags.vcf.gz" \
+     --threads $nr_threads -- -t MAF
 
 # Filter by MAF
-bcftools view -i "MAF>${min_maf}" \
-     "${out_dir}/tmp_${vcf_output}_noinfo_maf.vcf.gz" \
-     -Oz -o  "${out_dir}/tmp_${vcf_output}_noinfo_maf${min_maf}.vcf.gz" \
+bcftools view -i "MAF>${min_maf}" "${out_dir}/tmp_${vcf_prefix}_noinfo_filt_tags.vcf.gz" \
+     -Oz -o  "${out_dir}/tmp_${vcf_prefix}_noinfo_filt_tags_maf${min_maf}.vcf.gz" \
      --threads $nr_threads
 
 # Update variant IDs
-snp_id_vcf_file="${out_dir}/tmp_${vcf_output}_noinfo_maf${min_maf}_snp_id.vcf.gz"
+snp_id_vcf_file="${out_dir}/${vcf_prefix}_noinfo_filt_tags_maf${min_maf}_snp_id"
 bcftools annotate --set-id '%CHROM:%POS:%REF:%FIRST_ALT' \
-     "${out_dir}/tmp_${vcf_output}_noinfo_maf${min_maf}.vcf.gz" | \
-     bgzip -c > $snp_id_vcf_file
-tabix $snp_id_vcf_file
-
-# Filter to just samples of interest
-snp_id_vcf_file_filt="${out_dir}/${vcf_output}_noinfo_maf${min_maf}_snp_id_filt.vcf.gz"
-bcftools view -S "$samp_ids" \
-    $snp_id_vcf_file \
-    -Oz -o $snp_id_vcf_file_filt
-tabix $snp_id_vcf_file_filt
+    "${out_dir}/tmp_${vcf_prefix}_noinfo_filt_tags_maf${min_maf}.vcf.gz" \
+     --threads $nr_threads | \
+     bgzip -c > "${snp_id_vcf_file}.vcf.gz"
+tabix "${snp_id_vcf_file}.vcf.gz"
 
 # Clean up
 rm ${out_dir}/tmp_*
