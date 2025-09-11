bc9a89d81a7c43ce55e3a5cd336acab0f8c7f319
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-09-11 08:56:22 -0600

    Reorg to separate ancestry, continue removal of project-specific code

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ mv -r rfmix_input/ /path/to/repo
 
 # **Steps**
 
-## **local_anc_afr_eur**
+## **local_anc_afr_eur - RFMix**
 
 This sub-folder contains all the scripts needed to calculate two-way (AFR and
 EUR) local ancestry estimates using RFMix.
@@ -70,6 +70,21 @@ RFMix collapse option should be set to -o for uncollapsed output (used for LAAA
 fine mapping input) and set to -co for collapsed output (used for admixture
 mapping inout).
 
+## **gwide_anc**
+
+*TODO: add description*
+
+For admixture mapping, this full section needs to be run. If only want to run
+LAAA, then can just run script XXX.
+
+1. job_1.1_calc_rfmix_gwide_ancestry.batch, runs 1.1_calc_rfmix_gwide_ancestry.sh
+
+The first step calculates a global ancestry proportion based on the RFMix
+estimates which is used as a covariate in LAAA. For this first step to be run,
+you will have to already have results for all chromosomes from RFMix.
+
+*TODO: add discussion here for alternative if already have global ancestry.*
+
 ## **admixture_mapping**
 
 *TODO: to be added*
@@ -92,13 +107,7 @@ Many additional details are included in the bash and batch scripts.
 
 ### **To Process Phased Data (WGS or Imputed)**
 
-1. job_1.1_calc_rfmix_gwide_ancestry.batch, runs 1.1_calc_rfmix_gwide_ancestry.sh
 
-The first step calculates a global ancestry proportion based on the RFMix
-estimates which is used as a covariate in LAAA. For this first step to be run,
-you will have to already have results for all chromosomes from RFMix.
-
-*TODO: add discussion here for alternative if already have global ancestry.*
 
 *2. 2.1_make_laaa_pheno.R*
 
---
+++ gwide_anc/1.1_calc_gwide_ancestry.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+if [ "$#" -eq  "0" ]
+then
+   echo "Usage: ${0##*/} <plink_file_name> <rfmix_results_dir> <rfmix_input_dir>"
+   echo "	<admix_dir> <out_dir>"
+   echo "Script calculates ADMIXTURE and RFMix-based genome-wide ancestry."
+   exit
+fi
+
+# TO NOTE: pass in the paths to the PLINK1.9 file set used
+# as input to RFMIX with all variant IDs set to chr:pos:ref:alt (plink_file_name),
+# the path to RFMix results split by chromosome (rfmix_results dir), path to
+# the unzipped RFMix input directory containing reference files (rfmix_input_dir),
+# the path to the location to save ADMIXTURE inputs and outputs (admix_dir), and
+# path where want to save combined genome wide ancestry results (out_dir).
+
+# Scripts assume RFMix output file names are as output by RFMix:
+    # chr#_local_ancestry.0.Viterbi.txt
+    # chr#_local_ancestry.allelesRephased0.txt
+    # chr#_local_ancestry_snps.txt
+    # chr#_local_ancestry_samples.txt
+    # chr#_local_ancestry_batch1.0.SNPsPerWindow.txt (if found in dir, means
+        # RFMix was run with -co option)
+
+# Get args
+plink_file_name=$1
+rfmix_results_dir=$2
+rfmix_input_dir=$3  # genetic map & other unchanging inputs
+    #TODO: does this need to be clarified better?
+admix_dir=$4
+out_dir=$5
+
+# Calculate RFMIX genome-wide ancestry
+    # Third optional argument is SNPs per window file. Only need if RFMix
+    # was not run in batches as that means code won't auto find the file.
+Rscript calc_gwide_ancestry/calc_rfmix_gwide_ancestry.R \
+    ${plink_file_name}.fam $rfmix_results_dir
+    # ${plink_file_name}.fam $rfmix_results_dir "local_ancestry.0.SNPsPerWindow.txt"
+
+# Calculate ADMIXTURE genome-wide ancestry
+# Create working dir below rfmix_input_dir, delete at end
+rm -r "${rfmix_input_dir}/working"
+mkdir "${rfmix_input_dir}/working"
+# Create admixture dirs
+mkdir "${admix_dir}"
+admix_input_dir="${admix_dir}/input"
+mkdir "${admix_input_dir}"
+admix_out_dir="${admix_dir}/output"
+mkdir "${admix_out_dir}"
+
+# Make admixture inputs
+bash calc_gwide_ancestry/create_admixture_input.sh \
+    $plink_file_name $rfmix_results_dir $rfmix_input_dir $admix_input_dir
+
+# Run admixture with k = 2
+admixture "${admix_input_dir}/merged.bed" 2
+
+# Move results from working dir to output dir
+mv merged.* $admix_out_dir
+nr_samples=`wc -l ${plink_file_name}.fam | xargs | cut -f1 -d' '`
+Rscript calc_gwide_ancestry/calc_admixture_gwide_ancestry.R \
+    "${plink_file_name}.fam" "${admix_input_dir}/merged.fam" \
+    $admix_input_dir $admix_out_dir \
+    "${rfmix_input_dir}/igsr-1000_genomes_phase_3_release.tsv"
+
+#Create a combined genome-wide output file
+mkdir "${out_dir}"
+Rscript calc_gwide_ancestry/merge_gwide_ancestry.R \
+    $rfmix_results_dir $admix_out_dir $out_dir
+
+# Clean up
+rm -r "${rfmix_input_dir}/working"
